5fdefda704d488a183f830d090945f167d26c311
Maurine <maureen.wanjira@gmail.com>
2026-04-10 00:16:32 +0300

    Completed javascript  scope lab

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-let burgers = ["Hamburger", "Cheeseburger"];
-let featuredDrink = "Strawberry Milkshake";
+// Global scope variables
+let burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
 
+// Function-scoped variable inside function
 function addBurger() {
-  let newBurger = "Flatburger";
-  burgers.push(newBurger);
+    let newBurger = 'Flatburger';
+    burgers.push(newBurger);
 }
 
+// Block-scoped variable inside if statement
 if (true) {
-  let anotherNewBurger = "Maple Bacon Burger";
-  burgers.push(anotherNewBurger);
+    let anotherNewBurger = 'Maple Bacon Burger';
+    burgers.push(anotherNewBurger);
 }
 
+// Function to update global variable
 function changeFeaturedDrink() {
-  featuredDrink = "The JavaShake";
-}//  
+    featuredDrink = 'The JavaShake';
+}
